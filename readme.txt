4ff6421beb4f764ed2cffe2a46986d6910861ab7
Alexander Ivanov <yogaaaaa@yahoo.com>
2015-11-13 22:20:58 +0200

    adding session management. and db support. separated config module. added production and development configs.

--- readme.txt
+++ readme.txt
@@ -1,1 +1,3 @@
 Hello!
+to set process vars in node use:
+USER_ID=239482 USER_KEY=foobar node app.js
--- routes/router.js
+++ routes/router.js
@@ -5,5 +5,14 @@ module.exports = function(express, app) {
         res.render('index', { title: 'Mage Knight'});
     });
 
+    router.get('/setcolor', function(req, res, next) {
+        req.session.favColor = "Red";
+        res.send('Setting favourite colour !');
+    });
+
+    router.get('/getcolor', function(req, res, next) {
+        res.send('Favourite colour: ' + (req.session.favColor ? req.session.favColor : "Not Found"));
+    });
+
     app.use('/', router);
 }
