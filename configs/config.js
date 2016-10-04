c11c1ce45ddcb99d4ddacd40b6d9564de5ca88f1
Alexander Ivanov <alexander.ivanov01@sap.com>
2016-10-04 22:36:42 +0300

    no message in purpose

--- configs/config.js
+++ configs/config.js
@@ -1,1 +1,15 @@
-module.exports = require('./' + (process.env.NODE_ENV || 'development') + '.json');
+module.exports = function() {
+	function getEnvironmentVariables() {
+		return {
+			"dbURL": process.env.MONGODB_URI,
+			"sessionSecret": process.env.SESSION_SECRET,
+			"host": "https://" + process.env.APP_NAME + ".herokuapp.com"
+		}
+	}
+
+	if (process.env.NODE_ENV) { //production
+		return getEnvironmentVariables();
+	} else {
+		return require('./development.json');
+	}
+}();
